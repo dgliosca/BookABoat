6c596b125f2f8d7fda471144833aa6ea64cde960
diegogliosca <diego.gliosca@gmail.com>
2020-02-06 17:51:11 +0000

    even more tests

--- src/test/kotlin/BookingServiceTest.kt
+++ src/test/kotlin/BookingServiceTest.kt
@@ -4,43 +4,60 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class BookingServiceTest {
-    private val bookingService = BookingService(List(10) { Seat(it + 1) }.toSet())
+    private val boat = BookingService(List(10) { Seat(it + 1) }.toSet())
 
     @Test
     fun `retrieve available seats returns all seats`() {
-        val result = bookingService.availableSeats()
+        val result = boat.availableSeats()
         assertThat(result.size, equalTo(10))
     }
 
     @Test
     fun `book a seat`() {
-        assertTrue(bookingService.bookSeat(Seat(1)))
+        assertTrue(boat.bookSeat(Seat(1), name = "Diego"))
     }
 
     @Test
     fun `cannot book the same seat twice`() {
-        bookingService.bookSeat(Seat(1))
-        val bookSameSeat = bookingService.bookSeat(Seat(1))
-
+        boat.bookSeat(Seat(1), name = "Diego")
+        val bookSameSeat = boat.bookSeat(Seat(1), name = "Ben")
         assertThat(bookSameSeat, equalTo(false))
     }
 
     @Test
     fun `cannot book invalid seat`() {
         val invalidSeat = Seat(-1)
-        assertThat(bookingService.bookSeat(invalidSeat), equalTo(false))
+        assertThat(boat.bookSeat(invalidSeat, name = "Diego"), equalTo(false))
+    }
+
+    @Test
+    fun `retrieve passenger information`() {
+        boat.bookSeat(Seat(1), "Diego")
+        boat.bookSeat(Seat(2), "Ben")
+        assertThat(
+            boat.passengersInformation(), equalTo(
+                mapOf(Seat(1) to "Diego", Seat(2) to "Ben")
+            )
+        )
     }
 }
 
 data class Seat(val seatName: Int)
 
 class BookingService(private val allSeats: Set<Seat>) {
-    private val bookedSeats = mutableSetOf<Seat>()
+    private val bookedSeats = mutableMapOf<Seat, String>()
 
     fun availableSeats(): Set<Seat> {
-        return allSeats - bookedSeats
+        return allSeats - bookedSeats.keys
     }
 
-    fun bookSeat(seat: Seat) = if (allSeats.contains(seat)) bookedSeats.add(seat) else false
+    fun bookSeat(seat: Seat, name: String): Boolean =
+        if (allSeats.contains(seat) && !bookedSeats.contains(seat)) {
+            bookedSeats[seat] = name
+            true
+        } else false
 
+    fun passengersInformation(): Map<Seat, String> {
+        return bookedSeats
+    }
 }
